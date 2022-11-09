c5b7dfcb82c3615969e71123bbbe7371e1669eca
limhoontaig <limhoontaig@gmail.com>
2022-11-09 03:33:03 -0500

    add mysql select alias

--- mysql.php
+++ mysql.php
@@ -6,6 +6,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
   <title>Test Homepage for limhoontaig's Script</title>
   <h2> 임훈택의 시험 홈페이지 입니다. <br/> Hello PHP With MySQL</h2>
+  <!--<style type="test/css">
+  .collapse{
+  border-collapse: collapse;
+  border: 1px solid #ccc;
+  margin-left:10px;
+  }
+  .collapse th{
+  width: 30%;
+  padding: 5px;
+  background-color: #d8f550;
+  border: 1px solid #ccc;
+  }
+  .collapse td{
+  padding: 5px;
+  background-color: #fafafa;
+  border: 1px solid #ccc;
+  }
+ </style>-->
 </head>
 <body>
 <marquee direction="up" bgcolor="yellow" behavior="scroll"
@@ -54,16 +72,20 @@ if (mysqli_connect_errno()) {
 echo "test conection is success. <br/>";
 
 $query = "select 
-  e.emp_no, e.first_name, e.last_name, e.hire_date, s.salary, s.from_date, s.to_date, t.title, t.from_date, t.to_date
+  e.emp_no, e.first_name, e.last_name, e.hire_date, 
+  s.salary, s.from_date, s.to_date, 
+  t.title, t.from_date t_from_date, t.to_date t_to_date
 from 
   employees e,
   salaries s, 
   titles t
 WHERE
-  e.emp_no = s.emp_no
-  and e.emp_no = t.emp_no
-  and e.emp_no = $emp_no
-Order by e.emp_no";
+e.emp_no = s.emp_no
+and s.emp_no = t.emp_no
+and s.from_date >= t.from_date
+and s.from_date <= t.to_date
+and e.emp_no = $emp_no
+Order by s.from_date";
 echo "\$query test conection is success.  <br/>";
 $result = mysqli_query($con, $query);
 var_dump($result);
@@ -79,38 +101,36 @@ echo "while test conection is success. <br/>";
 if(mysqli_num_rows($result) > 0)
   {
   $table = '
-  <table border="1" bordercolor="red">
-                    <tr>
-                         <th> emp_no </th>
-                         <th>first_name</th>
-                         <th>last_name</th>
-                         <th>hire_date</th>
-                         <th>salary</th>
-                         <th>from_date</th>
-                         <th>to_date</th>
-                         <th>title</th>
-                         <th>t_from_date</th>
-                         <th>t_to_date</th>
-
-                    </tr>
+  <table border="1" bordercolor="red" border-collapse="collapse">
+  <!-- <table class="separate"> -->
+    <tr>
+      <th> emp_no </th>
+      <th>first_name</th>
+      <th>last_name</th>
+      <th>hire_date</th>
+      <th>salary</th>
+      <th>from_date</th>
+      <th>to_date</th>
+      <th>title</th>
+      <th>t_from_date</th>
+      <th>t_to_date</th>
+    </tr>
   ';
   while($row = mysqli_fetch_array($result))
   {
    $table .= '
     <tr>
-                        <td>'.$row["emp_no"].'</td>
-                         <td>'.$row["first_name"].'</td>
-                         <td>'.$row["last_name"].'</td>
-                         <td>'.$row["hire_date"].'</td>
-                         <td>'.$row["salary"].'</td>
-                         <td>'.$row["from_date"].'</td>
-                         <td>'.$row["to_date"].'</td>
-                         <td>'.$row["title"].'</td>
-                         <td>'.$row["from_date"].'</td>
-                         <td>'.$row["to_date"].'</td>
-
-
-                    </tr>
+      <td>'.$row["emp_no"].'</td>
+      <td>'.$row["first_name"].'</td>
+      <td>'.$row["last_name"].'</td>
+      <td>'.$row["hire_date"].'</td>
+      <td>'.$row["salary"].'</td>
+      <td>'.$row["from_date"].'</td>
+      <td>'.$row["to_date"].'</td>
+      <td>'.$row["title"].'</td>
+      <td>'.$row["t_from_date"].'</td>
+      <td>'.$row["t_to_date"].'</td>
+    </tr>
    ';
   }
   $table .= '</table>';
