73847ec3994858e4887ed1b30d2a3e3d55913bd7
limhoontaig <limhoontaig@gmail.com>
2022-11-11 07:49:40 +0900

    file name changed

--- study/test.php
+++ study/test.php
@@ -4,6 +4,15 @@
         <h1>임훈택의 집 방문해 주셔서 고맙습니다. 복많이 받으세요.</h1>
     </head>
     <body>
+        <?php
+        $connect= mysqli_connect("localhost","limht","secret","employees");
+        $emp_no = mysqli_query($connect, set @emp_no = (select max(emp_no) from employees) + 1);
+
+        echo "<h1> dump_var($emp_no) </h1>";
+        ?>
+
+
+
         <form action="./test2.php" name="form1" method="post">
             <input type="hidden" name="employees" value="employees">
                 <li> Employee Number: <input type="number" name="emp_no" value=""/></li>
