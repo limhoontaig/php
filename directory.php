6478516334c795896dd176fcd904be0f735b916d
limhoontaig <limhoontaig@gmail.com>
2022-11-15 20:43:32 -0500

    file link create with scandir

--- directory.php
+++ directory.php
@@ -2,25 +2,10 @@
 <?php
 $dir = getcwd();
 echo $dir ."<br>";
-$files = array();
-if (is_dir($dir)) {
-    if ($dh = opendir($dir)) {
-        while (($file = readdir($dh)) !== false) {
-            // echo "filename:".$file."<br>";
-            array_push($files, $file);
-        }
-        closedir($dh);
-        // print_r($files);
-    }
-}
-rsort($files);
-// $files_tag = array();
+$files = scandir($dir, 1); // 1 option descending order
+
 foreach ($files as $file) {
     $files_tag .= '<a href="'.$file. '"> Go To ' .$file.'<a/><br>';
-    // echo "<br>";
-    // echo "<a href=" $file ">Go to " $file "</a><br/>"
 }
 echo $files_tag;
 ?>
-
-CODE;
