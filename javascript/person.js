bccc3708427fef48c82f4420b0f73b61293495cf
limhoontaig <limhoontaig@gmail.com>
2022-11-23 07:10:39 +0900

    javascript tutorials file from w3school

---
+++ javascript/person.js
@@ -0,0 +1,9 @@
+// export all at once at the bottom:
+// const name = "Jesse";
+//const age = 40;
+
+//export {name, age};
+
+// export In-line individually
+export const name = "Jesse";
+export const age = 40;
