514caba4be677eed6e85c99d1723d5d20d0f091c
limhoontaig <limhoontaig@gmail.com>
2022-11-29 07:49:18 +0900

    mysql crud sample tutorial files

---
+++ crud_sample/procedual/config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'limht');
+define('DB_PASSWORD', 'secret');
+define('DB_NAME', 'crudSampleDB');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
