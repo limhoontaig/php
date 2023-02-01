b69054d0aa0eeef922b60d0a644e57f8728c5d40
limhoontaig <limhoontaig@gmail.com>
2023-02-01 03:23:54 -0500

    interim stage for file control

---
+++ file_control/index.php
@@ -0,0 +1,11 @@
+<html>
+    <meta charset="utf-8"/>
+    <title>File Manager</title>
+
+<body>
+    <form action="./upload.php" method="POST" enctype="multipart/form-data" >
+        <input type="file" id="file" name="file" required />
+        <input type="submit" value="UPLOAD" />
+    </form>
+<body>
+</html>
