c24b4a1f6dd007f369a3acddcb43ee68e0951ba3
limhoontaig <limhoontaig@gmail.com>
2023-02-02 03:35:19 -0500

    interim

--- file_control/upload.php
+++ file_control/upload.php
@@ -1,6 +1,7 @@
 <?php
     if(!$_FILES['file'] ['name'])
     {
+        
         echo "<script>alert ('업로드 할 파일이 입력되지 않았습니다.');";
         echo "history.back();</script>";
         exit;
@@ -12,21 +13,44 @@
         echo "history.back();</script>";
         exit;
     }
+    echo "$_FILES<br>";
+    $c = count($_FILES);
+    echo "Count of files : $c <br>"; 
+
 
     $date = date("YmdHis", time());
+    echo "Date is $date <br>";
     $dir = "./files/";
+    echo "$dir <br>";
     $file_hash = $date.$_FILES['file']['name'];
+    echo "$file_hash <br>";
     $file_hash = md5($file_hash);
+    echo "$file_hash <br>";
     $upfile = $dir.$file_hash;
-
-    if (is_uploaded_file($_FILES['file']['tmp_name']))
-    {
-        if(!move_uploaded_file($_FILES['file']['tmp_name'], $upfile))
+    echo "$upfile <br>";
+    print_r($_FILES);
+    if(move_uploaded_file($_FILES['file']['tmp_name'], $upfile))
         {
+            echo "$_FILES <br>";
+            echo "<br>";
+            echo "upload error 001";
+            // exit;
+            echo "File is valid, and was successfully uploaded.\n";
+    } else {
+            echo "Possible file upload attack!\n";
+    }
+    /*
+    if(is_uploaded_file($_FILES['file']['tmp_name']))
+    {
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $upfile))
+        {
+            echo "$_FILES <br>";
+            echo "<br>";
             echo "upload error 001";
             exit;
         }
     }
+    */
     @ $db = new mysqli("localhost","limht","secret","pictures");
     if (mysqli_connect_errno())
     {
@@ -48,5 +72,7 @@
 
     echo "<script>alert('업로드 성공');";
     echo "history.back();</script>";
+
+    
 
 ?>
